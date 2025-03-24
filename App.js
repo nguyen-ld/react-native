02630f24377ec6d5bf739301c03370b4a792e009
Le-Duc-Nguyen-10 <nguyenldpd10357@gmail.com>
2025-03-24 08:49:28 +0700

    update lab3

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { store } from "./lab3/redux/stores";
 import { Provider } from "react-redux";
 import Todos from "./lab3/Todo";
+import HomeScreen from "./lab3/Todo/home";
 export default function App() {
 	return (
 		<Provider store={store}>
-			<Todos />
+			<HomeScreen />
 		</Provider>
 	);
 }
